8bf4127df0699906875b534fdf0934ef2fbfdedc
yotaiyo <yota071218@gmail.com>
2019-07-11 17:56:21 +0900

    create card design

--- src/components/users.tsx
+++ src/components/users.tsx
@@ -1,29 +1,29 @@
 const user1 = {
-    icon: require('../../public/images/model_1.png'),
+    icon: '../../public/images/model_1.png',
     nickName: 'user1',
     age: 25
 }
 
 const user2 = {
-    icon: require('../../public/images/model_2.png'),
+    icon: '../../public/images/model_2.png',
     nickName: 'user2',
     age: 28
 }
 
 const user3 = {
-    icon: require('../../public/images/model_3.png'),
+    icon: '../../public/images/model_3.png',
     nickName: 'user3',
     age: 28
 }
 
 const user4 = {
-    icon: require('../../public/images/model_4.png'),
+    icon: '../../public/images/model_4.png',
     nickName: 'user4',
     age: 45
 }
 
 const user5 = {
-    icon: require('../../public/images/model_5.png'),
+    icon: '../../public/images/model_5.png',
     nickName: 'user5',
     age: 15
 }
