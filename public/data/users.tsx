499184c54b3343e20dcc933f4ded8dd9cbb4d39d
yotaiyo <yota071218@gmail.com>
2019-07-17 14:29:53 +0900

    modified users.tsx

--- public/data/users.tsx
+++ public/data/users.tsx
@@ -1,37 +1,37 @@
 const user1 = {
     icon: '../../public/images/model_1.jpeg',
-    nickName: 'Sophia',
-    age: 25
+    nickName: 'Jacob',
+    age: 42
 }
 
 const user2 = {
     icon: '../../public/images/model_2.jpeg',
-    nickName: '蓮',
-    age: 28
+    nickName: 'Jackson',
+    age: 32
 }
 
 const user3 = {
     icon: '../../public/images/model_3.jpeg',
-    nickName: '結月',
-    age: 28
+    nickName: 'Noah',
+    age: 26
 }
 
 const user4 = {
     icon: '../../public/images/model_4.jpeg',
-    nickName: 'Oliver',
-    age: 45
+    nickName: 'Sophia',
+    age: 29
 }
 
 const user5 = {
     icon: '../../public/images/model_5.jpeg',
-    nickName: 'さくら',
-    age: 15
+    nickName: 'Lucas',
+    age: 39
 }
 
 const user6 = {
     icon: '../../public/images/model_6.jpeg',
-    nickName: 'さくら',
-    age: 15
+    nickName: 'Chloe',
+    age: 30
 }
 
 export const users = [user1, user2, user3, user4, user5, user6]
