a08fcc055f994c7e7dfb0220a883f027caea9770
yotaiyo <yota071218@gmail.com>
2019-07-17 14:26:53 +0900

    modified user image

--- public/data/users.tsx
+++ public/data/users.tsx
@@ -1,31 +1,37 @@
 const user1 = {
-    icon: '../../public/images/model_1.png',
+    icon: '../../public/images/model_1.jpeg',
     nickName: 'Sophia',
     age: 25
 }
 
 const user2 = {
-    icon: '../../public/images/model_2.png',
+    icon: '../../public/images/model_2.jpeg',
     nickName: '蓮',
     age: 28
 }
 
 const user3 = {
-    icon: '../../public/images/model_3.png',
+    icon: '../../public/images/model_3.jpeg',
     nickName: '結月',
     age: 28
 }
 
 const user4 = {
-    icon: '../../public/images/model_4.png',
+    icon: '../../public/images/model_4.jpeg',
     nickName: 'Oliver',
     age: 45
 }
 
 const user5 = {
-    icon: '../../public/images/model_5.png',
+    icon: '../../public/images/model_5.jpeg',
     nickName: 'さくら',
     age: 15
 }
 
-export const users = [user1, user2, user3, user4, user5]
+const user6 = {
+    icon: '../../public/images/model_6.jpeg',
+    nickName: 'さくら',
+    age: 15
+}
+
+export const users = [user1, user2, user3, user4, user5, user6]
